e6fde9240096043a00dcbc6ebe8a5981b993d6f5
minahilmaroof <mmm@onyxtec.co>
2025-08-25 16:39:58 +0500

    text changes

--- src/views/screens/AuthScreens/LoginScreen.js
+++ src/views/screens/AuthScreens/LoginScreen.js
@@ -16,7 +16,6 @@ import CustomTextInput from '../../components/CustomTextInput';
 import AppButton from '../../components/AppButton';
 import CustomToast from '../../components/CustomToast';
 import colors from '../../../config/colors';
-import { fetchNotifications } from '../../../redux/slices/notificationSlice';
 const validationSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email address').required('Email is required'),
   password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
