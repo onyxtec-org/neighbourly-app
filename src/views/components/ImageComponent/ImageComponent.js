462379e3a8124f23d182e62c501d6b2eed25482e
maazafzal9 <mma@onyxtec.co>
2025-09-01 16:07:28 +0500

    update job details screen

--- src/views/components/ImageComponent/ImageComponent.js
+++ src/views/components/ImageComponent/ImageComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import FastImage from '@d11/react-native-fast-image';
+import { View, StyleSheet,Image } from 'react-native';
+//import FastImage from '@d11/react-native-fast-image';
 
-function ImageComponent({ source, style, height, width, resizeMode = FastImage.resizeMode.cover }) {
+function ImageComponent({ source, style, height, width, resizeMode = 'cover' }) {
   return (
     <View style={styles.container}>
-      <FastImage
+      <Image
         source={source}
         style={[
           { height: height || 50, width: width || 50 },
