b268a0758a97442d21f7b635ad81936b0714200e
maazafzal9 <mma@onyxtec.co>
2025-09-10 17:34:14 +0500

    update profile

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 import Config from "react-native-config";
 
 export default {
-  // baseURL: "https://nbl.onyxtec.io/api",
-  // userimageURL: "https://nbl.onyxtec.io/storage/user/",
-  // attachmentimageURL: "https://nbl.onyxtec.io/storage/job_attachments/",
-  // postAttachmentImageURL: "https://nbl.onyxtec.io/storage/post_attachments/",
-  // categoriesImageURL: "https://nbl.onyxtec.io/storage/categories/",
-  // serviceImageURL: "https://nbl.onyxtec.io/storage/services/",
+  baseURL: "https://nbl.onyxtec.io/api",
+  userimageURL: "https://nbl.onyxtec.io/storage/user/",
+  attachmentimageURL: "https://nbl.onyxtec.io/storage/job_attachments/",
+  postAttachmentImageURL: "https://nbl.onyxtec.io/storage/post_attachments/",
+  categoriesImageURL: "https://nbl.onyxtec.io/storage/categories/",
+  serviceImageURL: "https://nbl.onyxtec.io/storage/services/",
 
 //ngrok
-  baseURL: "https://eeaf49aa5ff4.ngrok-free.app/api",
-  userimageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/user/",
-  attachmentimageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/job_attachments/",
-  postAttachmentImageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/post_attachments/",
+  // baseURL: "https://eeaf49aa5ff4.ngrok-free.app/api",
+  // userimageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/user/",
+  // attachmentimageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/job_attachments/",
+  // postAttachmentImageURL: "https://eeaf49aa5ff4.ngrok-free.app/storage/post_attachments/",
 
 
 };
